3b096826fa9c8eec109d1e1fdfde136d86ab7489
Sharon M <sharonm2499@gmail.com>
2021-03-10 14:54:28 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+    var el = document.getElementById("present");
+    var cursore = document.getElementById("cursore");
+    cursore.style.visibility = "hidden";
+    el.style.visibility = "visible";
+}, 3000);
